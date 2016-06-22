0782b17e6069e8b311e9411592d346d09fac44fe
GauravKhupekar <gauravkh@thoughtworks.com>
2016-06-22 21:20:47 +0530

    added bridging-Header in project

---
+++ ScrewJackIntegration/ Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//   Bridging-Header.h
+//  ScrewJackIntegration
+//
+//  Created by GauravKhupekar on 6/22/16.
+//  Copyright © 2016 GauravKhupekar. All rights reserved.
+//
+
+#ifndef _Bridging_Header_h
+#define _Bridging_Header_h
+
+
+#endif /* _Bridging_Header_h */
