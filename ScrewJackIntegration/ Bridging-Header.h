16edbc0d070adf1bd51c3759a4cd52f954a93de8
GauravKhupekar <gauravkh@thoughtworks.com>
2016-06-24 17:15:57 +0530

    Added bridging header for fb authe. and DetailsViewController to go on next page on action of submit button

--- ScrewJackIntegration/ Bridging-Header.h
+++ ScrewJackIntegration/ Bridging-Header.h
@@ -1,13 +1,7 @@
-//
-//   Bridging-Header.h
-//  ScrewJackIntegration
-//
-//  Created by GauravKhupekar on 6/22/16.
-//  Copyright © 2016 GauravKhupekar. All rights reserved.
-//
-
 #ifndef _Bridging_Header_h
 #define _Bridging_Header_h
 
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
 
-#endif /* _Bridging_Header_h */
+#endif
